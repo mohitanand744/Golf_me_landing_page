f4c245f2d2f98f771d22a6086fdffeb1fdd681ef
mohitanand744 <jhamohit744@gmail.com>
2024-11-16 11:34:10 +0530

    added a new section

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,20 @@ function App() {
     <div className="Home">
       <Navbar />
       <HeroSection />
-      <div className="features bg-green-800 h-fit lg:h-[30rem] ">
-        <h1 className="text-center pt-5 text-4xl font-bold text-white">
+      <section className="features bg-green-800 h-fit lg:h-[30rem] ">
+        <h1 className="pt-5 text-4xl font-bold text-center text-white">
           Features
         </h1>
         <div className="grid grid-cols-12 h-[87%]">
-          <div className="col-span-12 md:col-span-6 relative">
-            <img
-              className="absolute bottom-0 left-0 hidden md:block"
-              src="/Features.png"
-              alt=""
-            />
+          <div className="col-span-12 md:col-span-6">
+            <div className="flex items-end justify-start h-full mt-5 md:mt-0 ">
+              <img className="" src="/Features.png" alt="" />
+            </div>
           </div>
 
           <div className="col-span-12 md:col-span-6 ">
             <div class="flex flex-col p-5 items-center justify-center gap-5 relative">
-              <div class="bg-white lg:absolute w-[100%] md:w-[25rem] lg:w-[30rem] mt-5 border-[5px] border-blue-400 rounded-3xl shadow-lg p-4 md:p-6  left-0 top-10 ">
+              <div class="bg-white lg:absolute w-[100%] md:w-[23rem] lg:w-[30rem] mt-5 border-[5px] border-blue-400 rounded-3xl shadow-lg p-4 md:p-6  left-0 top-10 ">
                 <h2 class="text-lg md:text-xl text-center font-semibold text-blue-600 mb-2">
                   Track Your Game
                 </h2>
@@ -33,7 +31,7 @@ function App() {
                 </ul>
               </div>
 
-              <div class="bg-white lg:absolute w-[100%] md:w-[25rem] lg:w-[30rem] mt-2 border-[5px] border-blue-400 rounded-3xl shadow-lg p-4 md:p-6   top-56 right-10">
+              <div class="bg-white lg:absolute w-[100%] md:w-[23rem] lg:w-[30rem] mt-2 border-[5px] border-blue-400 rounded-3xl shadow-lg p-4 md:p-6   top-56 right-10">
                 <h2 class="text-lg md:text-xl text-center font-semibold text-blue-600 mb-2">
                   Join Contest
                 </h2>
@@ -44,7 +42,54 @@ function App() {
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="howitworks bg-[#3793FB] py-8 md:py-10 px-6 md:px-24">
+        <h1 className="pb-6 text-4xl font-bold text-center text-white">
+          How it Works
+        </h1>
+
+        <p className="pb-16 text-xl text-center text-gray-100">
+          Simple, step-by-step visual guide:
+        </p>
+        <div className="grid grid-cols-12 gap-5">
+          <div className="col-span-12 md:col-span-6">
+            <div className="flex flex-col items-center justify-center mb-10 md:mb-0 md:h-[75%] gap-5">
+              <div className="text-black bg-white collapse collapse-arrow">
+                <input type="checkbox" />
+                <div className="text-lg font-medium md:text-xl collapse-title">
+                  Sign Up
+                </div>
+                <div className="collapse-content">
+                  <p>Create a free profile Fill All The Information.</p>
+                </div>
+              </div>
+              <div className="text-black bg-white collapse collapse-arrow">
+                <input type="checkbox" />
+                <div className="text-lg font-medium md:text-xl collapse-title">
+                  Track scores
+                </div>
+                <div className="collapse-content">
+                  <p>Content for the second accordion</p>
+                </div>
+              </div>
+              <div className="text-black bg-white collapse collapse-arrow">
+                <input type="checkbox" />
+                <div className="text-lg font-medium md:text-xl collapse-title">
+                  Join challenges
+                </div>
+                <div className="collapse-content">
+                  <p>Content for the third accordion</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-12 md:col-span-6">
+            <div className="flex items-center justify-center">
+              <img src="/mobile.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
